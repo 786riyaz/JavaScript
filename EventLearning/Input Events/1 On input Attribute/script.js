f48387115fa00fa51afe63a7ab97fda2c0fea5e6
Riyaz Pathan <moh.riyazpathan@gmail.com>
2023-09-23 15:52:48 +0530

    Added Text Input Event

---
+++ EventLearning/Input Events/1 On input Attribute/script.js
@@ -0,0 +1,8 @@
+console.clear();
+console.log("Application Started...");
+
+onInputAttributeFunction = function (input) {
+    console.log("~~~~~~~~Text Area on-Input-Attribute-Function is called~~~~~~~");
+    console.log("Entered value in Text Area ::: ");
+    console.log(input);
+}
