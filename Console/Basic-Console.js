cbca58fc1ac6486ee9772a9a1e8b284d155c4c7f
Riyaz Pathan <moh.riyazpathan@gmail.com>
2023-11-26 16:51:35 +0530

    Added console related information

---
+++ Console/Basic-Console.js
@@ -0,0 +1,14 @@
+console.log("Log Messages");            // Verbos
+console.info("Info Message");           // Info
+console.warn("Warning Message");        // Warning
+console.error("Error Message");         // Error
+console.debug("Debug Message");         // Debug
+
+// ---------------------------------------------------------------
+
+console.log("Normal, %c Green, %c Red ","color:green","background-color:red");           // Color and backgrounf in console
+
+// ---------------------------------------------------------------
+
+console.log(document.body);             // To print the HTML Element as HTML Format
+console.dir(document.body);             // To print the HTML Element with All the details
---
+++ Console/Console-Table.js
@@ -0,0 +1,16 @@
+let people = {
+    "Person1": {
+        "Name": "Riyaz",
+        "Age": 23,
+        "Address": "Danilida",
+        "Languages": ["C", "C++", "Java", "JavaScript"]
+    },
+    "Person2": {
+        "Name": "Arbaz",
+        "Age": 22,
+        "Address": "Danilida",
+        "Languages": ["Python", "VB", "Java"]
+    }
+}
+
+console.table(people);          // Displays all the details in the table format
