2c8f463f39f73530206f4d9f9d401d1f4a576442
Riyaz Pathan <moh.riyazpathan@gmail.com>
2023-12-17 18:12:49 +0530

    Object Method in JavaScript

---
+++ Object Function/Object Entry.js
@@ -0,0 +1,8 @@
+const Object1 = {
+    Name : "Riyaz",
+    Age : 24,
+    "Home Adress" : "Ahmedabad",
+    Languages : ["Java", "JavaScript", "Python"]
+}
+
+console.log(Object.entries(Object1));               // this will print array of all key-values in the objects
---
+++ Object Function/Object-in-JS.js
@@ -0,0 +1,8 @@
+const Object1 = {
+    Name : "Riyaz",
+    Age : 24,
+    "Home Adress" : "Ahmedabad",
+    Languages : ["Java", "JavaScript", "Python"]
+}
+
+console.log(Object1);               // this will print whole object
