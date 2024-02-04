63c5940e7115c955d56c2d467aa328a38b3c232b
Riyaz Pathan <moh.riyazpathan@gmail.com>
2024-02-04 16:51:42 +0530

    Added Output of Index of function

--- Array Function/Index Of/index of - Example 3.js
+++ Array Function/Index Of/index of - Example 3.js
@@ -1,7 +1,8 @@
+console.clear();
 let temp1 = "";
 
 if ([null, "null", "Null", undefined, "undefined", "Undefined", "", [], "[]", {}, "{}"].indexOf(temp1) == -1) {
     console.log("Element has some value.....");
 } else {
-    console.log("Element is either null, undefined or blank (has no value)");
+    console.log("Element is either null, undefined or blank (has no value)..........");
 }
