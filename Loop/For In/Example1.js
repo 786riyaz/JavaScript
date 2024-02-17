1e91dcbb4963b9a17d7a018450a3b761b0c87e18
Riyaz Pathan <moh.riyazpathan@gmail.com>
2024-02-18 00:09:17 +0530

    Added code for loop

---
+++ Loop/For In/Example1.js
@@ -0,0 +1,5 @@
+let array = [5, 10, 15, 20, 25, 30, 35, 40];
+
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
---
+++ Loop/For Of/Example1.js
@@ -0,0 +1,5 @@
+let array = [5, 10, 15, 20, 25, 30, 35, 40];
+
+for (let val of array){
+    console.log(val);
+}
