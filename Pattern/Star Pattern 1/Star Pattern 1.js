29393da39109412528fc1194616a2b061a1b4d9e
Riyaz Pathan <moh.riyazpathan@gmail.com>
2024-04-07 18:56:17 +0530

    Added Code for Star Pattern 1

---
+++ Pattern/Star Pattern 1/Star Pattern 1.js
@@ -0,0 +1,18 @@
+/*
+*******
+*******
+*******
+*******
+*******
+*******
+*******
+*/
+
+let finalMsessage = "";
+for(let i=0;i<7;i++){
+    for(let j=0;j<7;j++){
+        finalMsessage += "*";
+    }
+    finalMsessage += "\n";
+}
+console.log(finalMsessage);
