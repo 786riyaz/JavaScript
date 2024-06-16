4b114ace1f533fce3e9fe79a9a9e503b576b0f75
Riyaz Pathan <moh.riyazpathan@gmail.com>
2024-06-16 19:40:29 +0530

    Added Array Functions

---
+++ Array Functions/Find/find.js
@@ -0,0 +1,6 @@
+let array = ['A','D','B','C','D','E','F','D'];
+console.log("Array Before Reverse Operation ::: ");
+console.log(array);
+
+console.log("Array After Reverse Operation ::: ");
+console.log(array.find(e => (e == 'D')))
---
+++ Array Functions/Map/map.js
@@ -0,0 +1,6 @@
+let array = ['A','B','C','D','E','F'];
+console.log("Array Before Reverse Operation ::: ");
+console.log(array);
+
+console.log("Array After Reverse Operation ::: ");
+console.log(array.map(e => e + '1'))
