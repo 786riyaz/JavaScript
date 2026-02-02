49243b940f82fb9cdb6b0d2bbe16de1fb9bedb2d
Riyaz Pathan <moh.riyazpathan@gmail.com>
2026-02-02 15:17:46 +0530

    Added JavaScript Comment related Codes

---
+++ 02 JavaScript Basics/04 Comments/JS Doc Comments.js
@@ -0,0 +1,19 @@
+// The below comments written here allows any user to show the working of the function and meaning of parameters for this method.
+/**
+ * Adds two numbers and returns the sum.
+ * 
+ * @param {number} input1 - The first number.
+ * @param {number} input2 - The second number.
+ * @returns {number} The sum of input1 and input2.
+ */
+
+let sum = function(input1, input2) {
+    return input1 + input2;
+}
+
+// Declare variables a and b
+let a = 5;
+let b = 6;
+
+// Log the sum of a and b to the console
+console.log(sum(a, b));
---
+++ 02 JavaScript Basics/04 Comments/Single Line Comments.js
@@ -0,0 +1,3 @@
+// This is Single Line Comment
+let a = 5;      // Initializing A variable
+console.log (a)
