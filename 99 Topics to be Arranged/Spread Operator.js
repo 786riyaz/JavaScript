841b84df188539ee82eb088cc1af2c5afb97b828
Riyaz Pathan <moh.riyazpathan@gmail.com>
2026-02-03 16:13:04 +0530

    Added Spread Operator Code

---
+++ 99 Topics to be Arranged/Spread Operator.js
@@ -0,0 +1,8 @@
+let a = "Riyaz"
+let b = ["A", "V", 1, null]
+
+console.log(...a)
+console.log(...b)
+
+console.log(typeof [...a])
+// console.log(typeof ...a)                // SyntaxError: Unexpected token '...'
